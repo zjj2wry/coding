72007c36da1558621f6c8ca39ae06ccf45cc2201
zhengjiajin <zhengjiajin@caicloud.io>
2018-12-04 14:48:38 +0800

    add reflect

--- learn-go/reflect/relecte.go
+++ learn-go/reflect/relecte.go
@@ -21,11 +21,23 @@ func (o *order) do() {}
 func createQuery(q Order) {
 	t := reflect.TypeOf(q)
 	v := reflect.ValueOf(q)
+
 	fmt.Println("Type ", t)
 	fmt.Println("Value ", v)
 	fmt.Println("Element ", t.Elem())
+	// kind 获取 type 的类型
 	fmt.Println("Kind ", t.Kind(), t.Elem().Kind())
+	// name 获取 type 的名称
+	fmt.Println("Name ", t.Name(), "ss", t.Elem().Name())
 }
+
+/*
+Type  *main.order
+Value  &{456 56}
+Element  main.order
+Kind  ptr struct
+Name   ss order
+*/
 
 func main() {
 	o := &order{
